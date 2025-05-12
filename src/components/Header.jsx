0f9398d397604f09446694114cebf382020ed3e3
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-12 18:55:43 -0500

    header component that returns a nav element that renders Greeting and SocialLinks element

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Greeting from './Greeting';
+import SocialLinks from './SocialLinks';
+
+function Header() {
+  return (
+    <nav className="header-nav">
+      <Greeting />
+      <SocialLinks />
+    </nav>
+  );
+}
+
+export default Header;
