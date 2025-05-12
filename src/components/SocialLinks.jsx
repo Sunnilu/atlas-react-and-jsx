8f91234265bb36a14cffd67b45aa6d374b20aa69
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-12 18:52:17 -0500

    creating a social link that returns a div with a class name social-links

---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import linkedinIcon from '../assets/linkedin.svg';
+import githubIcon from '../assets/github.svg';
+
+function SocialLinks() {
+  return (
+    <div className="social-links">
+      <a
+        href="https://www.linkedin.com/in/suntha-lucas-657152290/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={linkedinIcon} alt="LinkedIn Profile" />
+      </a>
+      <a
+        href="https://github.com/Sunnilu/your-repo-name-here"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img src={githubIcon} alt="GitHub Repository" />
+      </a>
+    </div>
+  );
+}
+
+export default SocialLinks;
