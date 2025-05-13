5e5c63ae9434485db1470b63438cbc97bd105163
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-13 10:33:39 -0500

    updated App to with About Me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import Header from './components/Header';
 import Section from './components/Section';
+import AboutMe from './components/AboutMe';
 import HelpfulResource from './components/HelpfulResource';
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      {/* About Me Section */}
+      <AboutMe />
+
+      {/* What is React Section */}
+      <Section title="What is React?">
+        <p>
+          React is a JavaScript library for building fast and interactive user interfaces. It allows developers to create reusable components, manage application state efficiently, and deliver dynamic web experiences. Built by Facebook, React is declarative, component-based, and widely adopted for both personal and enterprise-grade projects.
+        </p>
+      </Section>
+
+      {/* Benefits of React Section */}
+      <Section title="Benefits of React">
+        <ul>
+          <li>Reusable components make development more efficient</li>
+          <li>Virtual DOM improves rendering performance</li>
+          <li>Strong community and ecosystem support</li>
+          <li>Declarative syntax makes code easier to read and debug</li>
+          <li>Great for building single-page applications (SPAs)</li>
+        </ul>
+      </Section>
 
       {/* Helpful Resources Section */}
       <Section title="Helpful resources">
