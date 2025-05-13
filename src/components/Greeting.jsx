8d0b6672229157fc03cf7168f2128969cb781f5a
Suntha <Suntha.Lucas@atlasschool.com>
2025-05-12 19:16:40 -0500

    returns an h1 element inside the h1 element render an image and some text that will change based on the time of day

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -5,27 +5,25 @@ import nightImg from '../assets/night.svg';
 
 function Greeting() {
   const hour = new Date().getHours();
-
-  let message = '';
-  let image = '';
+  let image, message;
 
   if (hour >= 6 && hour < 12) {
+    image = dayImg;
     message = 'Good Morning!';
-    image = dayImg;
   } else if (hour >= 12 && hour < 17) {
-    message = 'Good Afternoon!';
     image = dayImg;
+    message = 'Good Afternoon!';
   } else if (hour >= 17 && hour < 21) {
-    message = 'Good Evening!';
     image = eveningImg;
+    message = 'Good Evening!';
   } else {
-    message = 'Good Night!';
     image = nightImg;
+    message = 'Good Night!';
   }
 
   return (
     <h1 className="greeting">
-      <img src={image} alt="Greeting Time Visual" />
+      <img src={image} alt="Time of Day" />
       {message}
     </h1>
   );
